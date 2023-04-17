b150f88f8d83a86f1c0b04bfd7005abfe221859f
RizkyApiip <63902203+RizkyApiip@users.noreply.github.com>
2023-04-17 17:16:27 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// tombol class active
-const navbar = document.querySelector('nav .navbar');
-// ketika hamburger menu di pencet
-document.querySelector('#menubar').
-onclick = ( ) => {
-    navbar.classList.toggle('active');
-};
